2d5c7c6e3d39acbb9cf4d89cb6c1242617748286
Bruno Mendonça <68385709+BrunoMendoncaa@users.noreply.github.com>
2020-11-30 15:55:15 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php   // Conexão ao banco de dados 
-    
-    try{
-        $db = new PDO("mysql:dbname=inove;host=localhost","root","");
-    }catch(PDOException $e){
-        echo "Erro: " . $e->getMessage();
-    }
-?>
