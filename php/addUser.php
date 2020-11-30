6fa7db2239321b48531cca98959cc43efdf85fc4
SBKBS <brunomendonca011@gmail.com>
2020-11-30 16:55:58 -0300

    30.11.2020

--- php/addUser.php
+++ php/addUser.php
@@ -1,10 +1,14 @@
 <?php //Verificar se usuario já existe no banco de dados.
+    session_start();
+
     $sql = $db->prepare("SELECT * FROM login WHERE usuario = :u");
     $sql->bindValue(":u" , $user);
     $sql->execute();
 
     if($sql->rowcount() > 0){
-        header('location: ../index.html');
+        $_SESSION['cadastro'] = 'invalido';
+
+        header('location: ../index.php');
     }else{ //Adicionar usuario
         $sql = $db->prepare("INSERT INTO login (nome,usuario,senha,setor,adm) VALUES(:n,:u,:s,:st,0)");
         $sql->bindValue(":n"    ,  $nome);
@@ -12,5 +16,8 @@
         $sql->bindValue(":s"    , md5($pass));
         $sql->bindValue(":st"   , $setor);
         $sql->execute();
+
+        $_SESSION['cadastro'] = 'sucesso';
+        header('location: ../index.php');
     }
 ?>
